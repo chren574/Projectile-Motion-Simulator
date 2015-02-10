8efecefabeb73ddccced678d65b1744e171211c5
lasse93 <larbe444@student.liu.se>
2015-02-10 16:08:33 +0100

    flera simultana projektiler

--- webGL2/main.js
+++ webGL2/main.js
@@ -4,6 +4,7 @@ var WIDTH = 1000,
 var camera, scene, renderer;
 var geometry, material, mesh;
 
+var running;
 //Start varibles
 var velocity = 60;
 var time = 0;
@@ -15,6 +16,22 @@ var radius = 1.5;
 //init();
 animate();
 
+function update() {
+
+  window.alert("nej jag gor ingenting");
+
+}
+function launch() {
+
+  //window.alert("asdasd");
+  running = true;
+  animate();
+
+}
+function clearish() {
+  //window.alert("asdasdasdd");
+
+}
 function init() {
 
   var container = document.getElementById("container");
@@ -22,7 +39,6 @@ function init() {
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(WIDTH, HEIGHT);
   container.appendChild(renderer.domElement);
-
 
   camera = new THREE.PerspectiveCamera(75, WIDTH /HEIGHT , 1, 10000);
   scene = new THREE.Scene();
@@ -50,11 +66,6 @@ function init() {
   ball  = new THREE.Mesh(circleGeometry, material);
   scene.add(ball);
 
-  var initialVelocity = document.getElementById("initialVelocity").value;
-  var size = document.getElementById("size").value;
-
-  velocity = parseFloat(initialVelocity);
-
   //requestAnimationFrame(render);
 
 }
@@ -63,8 +74,39 @@ function animate() {
 
   requestAnimationFrame(animate);
 
+//Sätter nya variabler beroende på input
+  if(running == true)
+  {
+   // window.alert("asda");
+
+    var initialVelocity = document.getElementById("initialVelocity").value;
+    velocity = parseFloat(initialVelocity);
+    var angle = document.getElementById("angle").value;
+    ball_angle = parseFloat(angle);
+
+  
+    //*************************************************\\
+    /*
+      //väldigtväldigt fult men duger i krig 
+
+  size = document.getElementById("size").value;
+  radius = parseFloat(size);
+  var segments = 32;
+
+  var circleGeometry = new THREE.CircleGeometry( radius, segments );
+    var material = new THREE.MeshBasicMaterial({
+  color: 0x0000ff
+  });
+
+  //Add ball to scene
+  ball  = new THREE.Mesh(circleGeometry, material);
+  scene.add(ball);*/
 
 
+  //*************************************************\\    
+
+    running = false;
+  }
 /*
   cube.rotation.y += parseFloat(initialVelocity);
   cube.scale.x = parseFloat(size);
@@ -84,15 +126,22 @@ function animate() {
     time_old = 0;
   }
 
-  if (y <= -1)
+  if (y <= -0.01)
   {
+    time =0;
+    time_old = 5;
+    //kör oändligt om velocity inte sätts till 0
+    velocity = 0;
     cancelRequestAnimationFrame(animate);
+//--oklart om de ska sättas till 0, spelar ingen större roll--\\
+   //x = 0;
+   //y = 0;
+   // ball.position.x = 0;
+   // ball.position.y = 0;
   }
 
   time_old = time_old + 1;
   time = time + 0.05;
 
   renderer.render(scene, camera);
-
-}
-      
+}     
