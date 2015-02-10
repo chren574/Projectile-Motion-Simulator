f7291a5644d0ef6063e2bf37140d8afeeae882c6
johno855 <johno855@student.liu.se>
2015-02-10 14:04:26 +0100

    rensat och kommerterat lite i koden

--- MATLAB/simulering.m
+++ MATLAB/simulering.m
@@ -1,8 +1,3 @@
-% TNM085 - Modelleringsprojekt.
-% Simulering av kastbana.
-% 2015-02-10
-% 
-
 clear all;
 
 %%%%% Konstanter %%%%%
@@ -10,26 +5,24 @@ g = 9.82;                % Tyngdacceleration g [m/s^2]
 m = 0.145;               % Massa m [kg]
 r = 0.0366;              % Radie r [m]
 
+angle = 45;              % angle in degrees
+theta = angle*pi/180;    % convert to radians
+
 %%%%% Konstanter luftmotstand %%%%%
 A = r.^2*pi;             % Arean A [m^2]
 C = 0.5;
 p = 1.2;                 % Densiteten p []
 D = (p*C*A)/2;
 
-% initialv?rden
+% Konstanter
 v0 = 20;                 % initial hastigheten
-angle = 45;              % Vinkel  [grader]
-theta = angle*pi/180;    % Vinkel  [radianer]
+x(1) = 0;y(1) = 0;
+x_u(1) = 0;y_u(1) = 0;
 
-x(1) = 0;y(1) = 0;       % Startpositionen x och y-led.
-x_u(1) = 0;y_u(1) = 0;   % Startpositionen x och y-led.
-
-% Tidsvektorn for simuleringen
 t0 = 0;                  % starttid
 tf = 6.333 ;             % sluttid
-deltaT = 0.1;            % tidssteg --> andra for att se fel
-t = t0:deltaT:tf;        % tidsvektorn
-
+deltaT = 0.1;           % tidssteg
+t=t0:deltaT:tf ;         % tidsvektorn
 
 len=length(t);
 v=zeros(1, len);
@@ -46,21 +39,27 @@ ax_v= zeros(1, len);ay_v= zeros(1, len);
 vx_v=zeros(1, len); vy_v=zeros(1, len);
 x_v=zeros(1, len);  y_v=zeros(1, len);
 
-% Initial hastigheten x och y komponent.
+v(1) = v0*sin(theta);          %
+
+% Start hastigheten
 vx(1)   = v0*cos(theta);  vy(1) = v0*sin(theta);
 vx_u(1) = v0*cos(theta);vy_u(1) = v0*sin(theta);
 vx_v(1) = v0*cos(theta);vy_v(1) = v0*sin(theta);
 
 % Parametrar for vind
-U = 20;              % vindens hastighet
-Uang = 0;            % vindens vinkel
-
+% vindens hastighet
+U = 20;
+% vindens vinkel
+Uang = 0;
+ 
 % N = 100;
 % tmax = N*deltaT;
 
+%%
 
-for n = 2:len
 % for n = 1:1000
+for n = 2:len
+  
     
     %----------------------------------
     % Utan luftmotstand
@@ -80,7 +79,8 @@ for n = 2:len
     ax(n) =    -(D/m) * sqrt(vx(n-1)^2 + vy(n-1)^2)*vx(n-1);
     ay(n) = -g -(D/m) * sqrt(vx(n-1)^2 + vy(n-1)^2)*vy(n-1);
 %var1    ax(n) = (D*vx(n-1)*(sqrt(vx(n-1)^2+vy(n-1)^2)))/m;
-%var1    ay(n) = (D*vx(n-1)*(sqrt(vx(n-1)^2+vy(n-1)^2))-(m*g))/m;   
+%var1    ay(n) = (D*vx(n-1)*(sqrt(vx(n-1)^2+vy(n-1)^2))-(m*g))/m;
+    
     % Berknar hastigheten
     vx(n) = vx(n-1) + ax(n-1)*deltaT;
     vy(n) = vy(n-1) + ay(n-1)*deltaT;
@@ -112,6 +112,7 @@ for n = 2:len
 end
 
 plot(x_u, y_u, 'g', x, y,'r' , x_v, y_v, 'c');
+%plot(t, y,'r' , t, y_u, 'g');
 grid on;
 hold on;
 axis tight;
