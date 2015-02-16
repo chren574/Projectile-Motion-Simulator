de383724765c137651a7b74e00c82307f70a5e08
johno855 <johno855@student.liu.se>
2015-02-16 12:09:18 +0100

    added fps function and refactored the init function

--- webGL2/main.js
+++ webGL2/main.js
@@ -1,14 +1,17 @@
-// set the scene size
+// // set the scene size
+
+// TODO försök att ändra så storleken inte är konstant.
 var WIDTH = 1000,
     HEIGHT = 600;
 
-// set some camera attributes
-var VIEW_ANGLE = 75,
+  // set some camera attributes
+  var VIEW_ANGLE = 75,
   ASPECT = WIDTH / HEIGHT,
   NEAR = 1,
   FAR = 10000;
 
-var camera, scene, renderer;
+
+var camera, scene, renderer, stats;
 var geometry, material, mesh;
 var running;
 
@@ -20,11 +23,7 @@ var ball_angle = 70;
 var gravity = 9.8;
 var radius = 1.5;
 
-<<<<<<< HEAD
 //init();
-=======
-init();
->>>>>>> 7b8ca1ebe709780a7c065ef4c88dd70cdcd28159
 //animate();
 
 function update() {
@@ -44,15 +43,16 @@ function clearish() {
 
 }
 function init() {
-  // get the DOM element to attach to
-  var container = document.getElementById("container");
+  
+  //------------------------------------------------------
+  // SCENE 
+  scene = new THREE.Scene();
 
-  // create a WebGL renderer, camera
-  renderer = new THREE.WebGLRenderer();
+  //------------------------------------------------------
+  // CAMERA
 
   // create camera and a scene
   camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT , NEAR, FAR);
-  scene = new THREE.Scene();
 
   // add the camera to the scene
   scene.add(camera);
@@ -61,11 +61,49 @@ function init() {
   // so pull it back
   camera.position.z = 200;
 
+  //------------------------------------------------------
+  // RENDERER
+
+  // create a WebGL renderer, camera
+  renderer = new THREE.WebGLRenderer();
+
   // start the renderer
   renderer.setSize(WIDTH, HEIGHT);
 
+  // get the DOM element to attach to
+  var container = document.getElementById("container");
+
   // attach the render-supplied DOM element
-  container.appendChild(renderer.domElement);
+  container.appendChild( renderer.domElement );
+
+  // renderer.setSize(container.offsetWidth, container.offsetHeight);
+  // container.appendChild(renderer.domElement);
+
+  // var HEIGHT = document.getElementById('container').clientHeight;
+  // var WIDTH = document.getElementById('container').clientWidth;
+
+  //------------------------------------------------------
+  // STATS 
+
+  // displays current and past frames per second attained by scene
+  stats = new Stats();
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.bottom = '0px';
+  stats.domElement.style.zIndex = 100;
+
+  container.appendChild( stats.domElement );
+
+  //------------------------------------------------------
+  // LIGHT
+
+
+
+  //------------------------------------------------------
+  // GEOMETRY
+
+  // most objects displayed are a "mesh":
+  // a collection of points ("geometry") and
+  // a set of surface parameters ("material") 
 
   //Plane geometry and material
   var geometry = new THREE.PlaneGeometry( 5000, 1, 32 );
@@ -77,12 +115,9 @@ function init() {
   //var radius = 5;
   var segments = 32;
   var circleGeometry = new THREE.CircleGeometry( radius, segments );
-
-
   var material = new THREE.MeshBasicMaterial({
   color: 0x0000ff
   });
-
   //Add ball to scene
   ball  = new THREE.Mesh(circleGeometry, material)
   scene.add(ball);
@@ -93,24 +128,21 @@ function init() {
 }
 
 function animate() {
+  
   requestAnimationFrame(animate);
 
-<<<<<<< HEAD
   //Sätter nya variabler beroende på input
-  if(running == true) {
+  if( running ) {
   // window.alert("asda");
-=======
-//Sätter nya variabler beroende på input
-  
-  if(running == true)
-  {
-   // window.alert("asda");
->>>>>>> 7b8ca1ebe709780a7c065ef4c88dd70cdcd28159
-
     var initialVelocity = document.getElementById("initialVelocity").value;
     velocity = parseFloat(initialVelocity);
+    
     var angle = document.getElementById("angle").value;
     ball_angle = parseFloat(angle);
+
+    time = 0;
+
+    console.log("The time is set: " + time)
 
   
     //*************************************************\\
@@ -132,43 +164,40 @@ function animate() {
 
 
   //*************************************************\\    
-
     running = false;
   }
-/*
-  cube.rotation.y += parseFloat(initialVelocity);
-  cube.scale.x = parseFloat(size);
-  cube.scale.y = parseFloat(size);
-  cube.scale.z = parseFloat(size);*/
 
-  ball.position.x = LIB.distX(velocity,ball_angle,time) -160;
-  ball.position.y = LIB.distY(velocity,ball_angle,time, gravity);
+  ball.position.x = LIB.distX(velocity, ball_angle, time) - 160;
+  ball.position.y = LIB.distY(velocity, ball_angle, time, gravity);
 
   x = ball.position.x;
   y = ball.position.y;
 
-  if(time_old == 5) {  
-    var circle = ball.clone();
-    circle.position.set(x, y, 0);
-    scene.add(circle);
-    time_old = 0;
-  }
+//   if(time_old == 5) {  
+//     var circle = ball.clone();
+//     circle.position.set(x, y, 0);
+//     scene.add(circle);
+//     time_old = 0;
+//   }
 
-  if (y <= -0.01) {
-    time =0;
-    time_old = 5;
-    //kör oändligt om velocity inte sätts till 0
-    velocity = 0;
-    cancelRequestAnimationFrame(animate);
-//--oklart om de ska sättas till 0, spelar ingen större roll--\\
-   //x = 0;
-   //y = 0;
-   // ball.position.x = 0;
-   // ball.position.y = 0;
-  }
+//   if (y <= -0.01) {
+//     time =0;
+//     time_old = 5;
+//     //kör oändligt om velocity inte sätts till 0
+//     velocity = 0;
+//     cancelRequestAnimationFrame(animate);
+//    //--oklart om de ska sättas till 0, spelar ingen större roll--\\
+//    //x = 0;
+//    //y = 0;
+//    // ball.position.x = 0;
+//    // ball.position.y = 0;
+//   }
 
   time_old = time_old + 1;
   time = time + 0.05;
 
+  stats.update();
+
   renderer.render(scene, camera);
+
 }     
