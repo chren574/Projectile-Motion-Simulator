eb9b8bdf618d8085e25ca43c17ac6786ffdfe348
chren574 <chren574@student.liu.se>
2015-02-17 10:05:40 +0100

    optimized the kod and fix the FPS-problem

--- webGL2/main.js
+++ webGL2/main.js
@@ -10,21 +10,16 @@ var WIDTH = 1000,
   NEAR = 1,
   FAR = 10000;
 
-
 var camera, scene, renderer, stats;
 var geometry, material, mesh;
 var running;
 
 //Start varibles
 var velocity = 40;
-var time = 0;
 var time_old = 5;
 var ball_angle = 70;
 var gravity = 9.8;
 var radius = 1.5;
-
-//init();
-//animate();
 
 function launch() {
 
@@ -34,6 +29,7 @@ function launch() {
 
 }
 function clearish() {
+
   //window.alert("asdasdasdd");
 
 }
@@ -75,12 +71,6 @@ function init() {
   // attach the render-supplied DOM element
   container.appendChild( renderer.domElement );
 
-  // renderer.setSize(container.offsetWidth, container.offsetHeight);
-  // container.appendChild(renderer.domElement);
-
-  // var HEIGHT = document.getElementById('container').clientHeight;
-  // var WIDTH = document.getElementById('container').clientWidth;
-
   //------------------------------------------------------
   // STATS 
 
@@ -114,49 +104,53 @@ function init() {
   //Plane geometry and material
   var geometry = new THREE.PlaneGeometry( 500, 300, 20 );
   var material = new THREE.MeshPhongMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-  plane = new THREE.Mesh( geometry, material );
+  var plane = new THREE.Mesh( geometry, material );
   plane.rotation.x = Math.PI/2;
   scene.add( plane );
 
   //Ball geometry and material
-
-
   var spheregeometry = new THREE.SphereGeometry( 15, 32, 32 );
-
-  var material = new THREE.MeshPhongMaterial({
-  color: 0x0033ff
-  });
+  var material = new THREE.MeshPhongMaterial( {color: 0x0033ff } );
   ball  = new THREE.Mesh(spheregeometry, material);
   ball.position.y = 10;
   scene.add(ball);
 
 
   renderer.render(scene, camera);
-  //requestAnimationFrame(render);
 
 }
 function animate() {
 
-  requestAnimationFrame(animate);
+  id = requestAnimationFrame(animate);
   render();
   stats.update();
 }
 function render() {
 
-  //ball.position.x += 0.5;
-
   ball.position.x = LIB.distX(velocity, ball_angle, time) - 160;
   ball.position.y = LIB.distY(velocity, ball_angle, time, gravity);
 
+  if(time_old == 30) {  
+     var circle = ball.clone();
+     circle.position.set(ball.position.x, ball.position.y, 0);
+     scene.add(circle);
+     time_old = 0;
+   }
+
+  time_old = time_old + 1;
   time = time + 0.05;
+
+  if (ball.position.y < 0)
+  {
+    cancelAnimationFrame( id );
+  }
+
   renderer.render(scene, camera);
+
 }
 
-/*
-function animate() {
   
-
-  requestAnimationFrame(animate);
+/*
 
   //Sätter nya variabler beroende på input
   if( running ) {
@@ -173,39 +167,3 @@ function animate() {
 
     running = false;
   }
-
-  ball.position.x = LIB.distX(velocity, ball_angle, time) - 160;
-  ball.position.y = LIB.distY(velocity, ball_angle, time, gravity);
-
-  x = ball.position.x;
-  y = ball.position.y;
-
-//   if(time_old == 5) {  
-//     var circle = ball.clone();
-//     circle.position.set(x, y, 0);
-//     scene.add(circle);
-//     time_old = 0;
-//   }
-
-//   if (y <= -0.01) {
-//     time =0;
-//     time_old = 5;
-//     //kör oändligt om velocity inte sätts till 0
-//     velocity = 0;
-//     cancelRequestAnimationFrame(animate);
-//    //--oklart om de ska sättas till 0, spelar ingen större roll--\\
-//    //x = 0;
-//    //y = 0;
-//    // ball.position.x = 0;
-//    // ball.position.y = 0;
-//   }
-
-  time_old = time_old + 1;
-  time = time + 0.05;
-
-  stats.update();
-
-  renderer.render(scene, camera);
-
-}    
-*/
